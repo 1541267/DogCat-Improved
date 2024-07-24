f9328980888d4091f0ad27990b0444a264971efb
1541267 <1541267@naver.com>
2024-07-24 23:52:57 +0900

    EC2 준비

--- src/main/java/com/community/dogcat/config/CustomServletConfig.java
+++ src/main/java/com/community/dogcat/config/CustomServletConfig.java
@@ -1,6 +1,7 @@
 package com.community.dogcat.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -22,5 +23,12 @@ public class CustomServletConfig implements WebMvcConfigurer {
 		registry.addResourceHandler("/s3/**").addResourceLocations("file:///C:/testupload/s3/");
 		registry.addResourceHandler("/temp/**").addResourceLocations("file:///C:/testupload/temp/");
 	}
-
+	// CORS
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("https://dogcat.n-e.kr")
+			.allowedMethods("GET", "POST", "PUT", "DELETE")
+			.allowedHeaders("*");
+	}
 }
